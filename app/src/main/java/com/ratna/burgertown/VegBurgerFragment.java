2a6e4849c3c3bfd6437c716d2309fc2a2ff445b3
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-26 22:32:40 +0545

    fifth commit

--- app/src/main/java/com/ratna/burgertown/VegBurgerFragment.java
+++ app/src/main/java/com/ratna/burgertown/VegBurgerFragment.java
@@ -2,13 +2,31 @@ package com.ratna.burgertown;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class VegBurgerFragment extends Fragment {
+    LinearLayout saladLayout, baconLayout, cheeseLayout, PattyLayout;
+    CardView addSalad, removeSalad, addCarrotBacon, removeCarrotBacon, addCheese, removeCheese, addPatty, removePatty;
+    Button orderButton;
+    TextView totalPriceText;
+    int bunPrice = 55;
+    int saladPrice = 10;
+    int carrotBaconPrice = 10;
+    int cheesePrice = 20;
+    int pattyPrice = 15;
+    int totalPrice;
+    int saladCount = 0;
+    int carrotBaconCount = 0;
+    int cheeseCount = 0;
+    int pattyCount = 0;
 
     public VegBurgerFragment() {
         // Required empty public constructor
