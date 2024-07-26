d22008352a27f61b0b038ac42919944a94f0d555
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-26 22:29:20 +0545

    fourth commit

--- app/src/main/java/com/ratna/burgertown/BurgerActivity.java
+++ app/src/main/java/com/ratna/burgertown/BurgerActivity.java
@@ -1,24 +1,44 @@
 package com.ratna.burgertown;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class BurgerActivity extends AppCompatActivity {
+    RadioButton nonVegBurgerRadioButton, vegBurgerRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_burger);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nonVegBurgerRadioButton = findViewById(R.id.nonVegBurgerRadioButton);
+        vegBurgerRadioButton = findViewById(R.id.vegBurgerRadioButton);
+
+        nonVegBurgerRadioButton.setOnClickListener(view -> {
+            NonVegBurgerFragment nonVegBurgerFragment = new NonVegBurgerFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.burgerFrame, nonVegBurgerFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+
+        vegBurgerRadioButton.setOnClickListener(view -> {
+            VegBurgerFragment vegBurgerFragment = new VegBurgerFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.burgerFrame, vegBurgerFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
         });
     }
 }
--- app/src/main/java/com/ratna/burgertown/NonVegBurgerFragment.java
+++ app/src/main/java/com/ratna/burgertown/NonVegBurgerFragment.java
@@ -1,64 +1,243 @@
 package com.ratna.burgertown;
 
+import static android.util.TypedValue.applyDimension;
+
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NonVegBurgerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NonVegBurgerFragment extends Fragment {
+    LinearLayout saladLayout, baconLayout, cheeseLayout, meatLayout;
+    CardView addSalad, removeSalad, addBacon, removeBacon, addCheese, removeCheese, addMeat, removeMeat;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    AlertDialog alert;
+    Button orderButton;
+    TextView totalPriceText;
+    int bunPrice = 55;
+    int saladPrice = 10;
+    int baconPrice = 15;
+    int cheesePrice = 20;
+    int meatPrice = 25;
+    int totalPrice;
+    int saladCount = 0;
+    int baconCount = 0;
+    int cheeseCount = 0;
+    int meatCount = 0;
 
     public NonVegBurgerFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NonVegBurgerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NonVegBurgerFragment newInstance(String param1, String param2) {
-        NonVegBurgerFragment fragment = new NonVegBurgerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_non_veg_burger, container, false);
+        View view = inflater.inflate(R.layout.fragment_non_veg_burger, container, false);
+        saladLayout = view.findViewById(R.id.saladLayout);
+        baconLayout = view.findViewById(R.id.baconLayout);
+        cheeseLayout = view.findViewById(R.id.cheeseLayout);
+        meatLayout = view.findViewById(R.id.meatLayout);
+        addSalad = view.findViewById(R.id.addSalad);
+        removeSalad = view.findViewById(R.id.removeSalad);
+        addBacon = view.findViewById(R.id.addBacon);
+        removeBacon = view.findViewById(R.id.removeBacon);
+        addCheese = view.findViewById(R.id.addCheese);
+        removeCheese = view.findViewById(R.id.removeCheese);
+        addMeat = view.findViewById(R.id.addMeat);
+        removeMeat = view.findViewById(R.id.removeMeat);
+        totalPriceText = view.findViewById(R.id.totalPriceText);
+        orderButton = view.findViewById(R.id.orderBtn);
+
+        addSalad.setOnClickListener(view1 -> {
+            saladCount++;
+            changeSalad();
+        });
+
+        removeSalad.setOnClickListener(view1 -> {
+            saladCount--;
+            if (saladCount < 0) {
+                saladCount = 0;
+            }
+            changeSalad();
+        });
+
+        addBacon.setOnClickListener(view1 -> {
+            baconCount++;
+            changeBacon();
+        });
+
+        removeBacon.setOnClickListener(view1 -> {
+            baconCount--;
+            if (baconCount < 0) {
+                baconCount = 0;
+            }
+            changeBacon();
+        });
+
+        addCheese.setOnClickListener(view1 -> {
+            cheeseCount++;
+            changeCheese();
+        });
+
+        removeCheese.setOnClickListener(view1 -> {
+            cheeseCount--;
+            if (cheeseCount < 0) {
+                cheeseCount = 0;
+            }
+            changeCheese();
+        });
+
+        addMeat.setOnClickListener(view1 -> {
+            meatCount++;
+            changeMeat();
+        });
+
+        removeMeat.setOnClickListener(view1 -> {
+            meatCount--;
+            if (meatCount < 0) {
+                meatCount = 0;
+            }
+            changeMeat();
+        });
+
+        orderButton.setOnClickListener(view1 -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Order Confirmation");
+            builder.setCancelable(false);
+
+            LayoutInflater layoutInflater = getLayoutInflater();
+            View view2 = layoutInflater.inflate(R.layout.item_ordered_non_veg, null);
+
+            TextView saladQuantity = view2.findViewById(R.id.sQty);
+            saladQuantity.setText(String.valueOf(saladCount));
+
+            TextView saladPriceText = view2.findViewById(R.id.saladPrice);
+            saladPriceText.setText(String.valueOf(saladCount * saladPrice));
+
+            Button confirm = view2.findViewById(R.id.btnConfirm);
+            confirm.setOnClickListener(view3 -> {
+                alert.dismiss();
+            });
+
+            builder.setView(view);
+            alert = builder.create();
+            alert.show();
+        });
+
+        return view;
+    }
+
+    void changeSalad() {
+        saladLayout.removeAllViews();
+        for (int i = 0; i < saladCount; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(R.drawable.salad_shape);
+            int heightInPixels = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    20,
+                    getResources().getDisplayMetrics()
+            );
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    heightInPixels
+            );
+
+            imageView.setLayoutParams(params);
+            saladLayout.addView(imageView);
+        }
+        updateTotalPrice();
+    }
+
+    void changeBacon() {
+        baconLayout.removeAllViews();
+        for (int i = 0; i < baconCount; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(R.drawable.bacon_shape);
+            int heightInPixels = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    10,
+                    getResources().getDisplayMetrics()
+            );
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    heightInPixels
+            );
+
+            int marginInPixels = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    10,
+                    getResources().getDisplayMetrics()
+            );
+
+            params.leftMargin = marginInPixels;
+            params.rightMargin = marginInPixels;
+
+            imageView.setLayoutParams(params);
+            baconLayout.addView(imageView);
+        }
+        updateTotalPrice();
+    }
+
+    void changeCheese() {
+        cheeseLayout.removeAllViews();
+for (int i = 0; i < cheeseCount; i++) {
+    ImageView imageView = new ImageView(getActivity());
+    imageView.setBackgroundResource(R.drawable.cheese_shape);
+    int heightInPixels = (int) TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            15,
+            getResources().getDisplayMetrics()
+    );
+
+    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            heightInPixels
+    );
+
+    imageView.setLayoutParams(params);
+    cheeseLayout.addView(imageView);
+}
+updateTotalPrice();
+    }
+
+    void changeMeat(){
+        meatLayout.removeAllViews();
+        for (int i = 0; i < meatCount; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(R.drawable.meat_shape);
+            int heightInPixels = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    35,
+                    getResources().getDisplayMetrics()
+            );
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    heightInPixels
+            );
+
+            imageView.setLayoutParams(params);
+            meatLayout.addView(imageView);
+        }
+        updateTotalPrice();
+    }
+
+    void updateTotalPrice() {
+        totalPrice = bunPrice + (saladCount * saladPrice) + (baconCount * baconPrice) + (cheeseCount * cheesePrice) + (meatCount * meatPrice);
+        totalPriceText.setText(String.valueOf(totalPrice));
     }
 }
